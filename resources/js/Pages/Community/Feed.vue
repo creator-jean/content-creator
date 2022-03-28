05c864ba6d68041b34fe9428cd2cdb92dd5a310c
rcmadeloso <sr.rcmadeloso@gmail.com>
2022-03-28 13:29:14 +0800

    first commit

---
+++ resources/js/Pages/Community/Feed.vue
@@ -0,0 +1,41 @@
+<template>
+
+<community-layout>
+	
+<div class="mcontainer"> <!-- Start -->
+
+                <div class="flex justify-between relative md:mb-4 mb-3">
+                    <div class="flex-1">
+                        <h2 class="text-2xl font-semibold"> Upload your content </h2>
+                    </div>
+                </div>
+
+                <br>  
+
+                <div class="divide-y">
+
+
+                    <div class="flex md:space-x-6 space-x-4 md:py-5 py-3 relative">
+                        
+                    </div> 
+
+                </div> 
+        
+            </div> <!-- End -->
+	
+</community-layout>	
+            
+	
+</template>
+
+
+<script>
+    import { defineComponent } from 'vue'
+    import CommunityLayout from '@/Layouts/CommunityLayout.vue'
+
+    export default defineComponent({
+        components: {
+            CommunityLayout,
+        },
+    })
+</script>
---
+++ resources/views/community.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from demo.foxthemes.net/socialitev2.2/albums.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 27 Mar 2022 09:26:58 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicon -->
+    <link href="images/favicon.png" rel="icon" type="image/png">
+
+    <!-- Basic Page Needs
+        ================================================== -->
+    <title>Socialite Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Socialite is - Professional A unique and beautiful collection of UI elements">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <!-- Scripts -->
+    @routes
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+</head> 
+<body>
+    @inertia
+    
+    <!-- For Night mode -->
+  
+    <!-- Javascript
+    ================================================== -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="js/tippy.all.min.js"></script>
+    <script src="js/uikit.js"></script>
+    <script src="js/simplebar.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/bootstrap-select.min.js"></script>
+    <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+
+</body>
+
+<!-- Mirrored from demo.foxthemes.net/socialitev2.2/albums.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 27 Mar 2022 09:26:58 GMT -->
+</html>
